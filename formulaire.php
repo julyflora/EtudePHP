a8761241b004c210a6cb5371d18920f4441949d6
july floraa <floratatchou@gmail.com>
2020-03-16 17:55:38 +0100

    initialisation du projet

---
+++ formulaire.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil|Bienvenue sur mon Blog</title>
+</head>
+<body>
+
+    
+</body>
+</html>
